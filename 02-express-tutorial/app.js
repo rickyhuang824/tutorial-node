88c0a6bda414a391e67639ec7ac3c2a824afbfc8
ricky huang <ricky.huang@cfusacorp.com>
2021-11-01 22:14:39 -0700

    learn app.use to apply the middleware to multiple routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
+const logger = require('./logger');
 
+app.use('/api', logger);
 // req => middleware => res
-
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method,url,time);
-    next();
-}
-
-app.get('/', logger, (req, res)=>{
+app.get('/', (req, res)=>{
     res.send('Home');
 })
 
-
 app.get('/about', (req, res)=>{
     res.send('About');
+});
+
+app.get('/api/products', (req, res)=>{
+    res.send('Products');
+})
+
+app.get('/api/items', (req, res)=>{
+    res.send('Items');
 });
 
 app.listen(5000, ()=>{
