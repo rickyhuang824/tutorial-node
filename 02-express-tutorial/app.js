121b807e2972a0c935bbe2c56c836e26ffbefe9d
ricky huang <ricky.huang@cfusacorp.com>
2021-11-02 23:14:05 -0700

    Post data using javascript

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,9 +5,18 @@ let { people } = require('./data');
 
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/api/people',(req,res)=>{
     res.status(200).json({data: people, sucess: true});
+});
+
+app.post('/api/people', (req, res)=>{
+    const { name } = req.body;
+    if(!name) {
+        return res.status(400).json({sucess:false, msg: 'please provide name value'});
+    }
+   res.status(201).send({sucess:true, person: name});
 });
 
 app.post('/login', (req, res) => {
